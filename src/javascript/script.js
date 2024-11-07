0a8f349dc95d85bb0623a45f4eef34d0da092dda
Gabrielly <gabriellydiaspereira15@gmail.com>
2024-11-07 20:26:08 -0300

    ajustando imagem home

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -13,12 +13,6 @@ $(document).ready(function() {
 
         let activeSectionIndex = 0
 
-        if(scrollPosition <= 0) {
-            header.css('box-shadow', 'none')
-        } else {
-            header.css('box-shadow', '5px 1px 5px rgba(0, 0, 0, 0.1')
-        }
-
         sections.each(function(i) {
             const section = $(this)
             const sectionTop = section.offset().top - 96
